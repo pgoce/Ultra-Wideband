0ebaeabe7c84bf01fc67ee80bfe0b911a81ecb8b
parcosp <pa344@njit.edu>
2022-10-28 11:11:16 -0400

    Initial Commit

---
+++ UltraWideband/UltraWidebandApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltraWidebandApp.swift
+//  UltraWideband
+//
+//  Created by Paul Arcos on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct UltraWidebandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
